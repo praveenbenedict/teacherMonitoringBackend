43b736cf91d9402f2d00335074eeb3c24580080c
Naveen Alphonse <jbapraveen@hotmail.com>
2018-08-27 18:17:26 +0530

    Fixed Bugs

--- app.js
+++ app.js
@@ -1,16 +1,26 @@
-//Leander Paul
-
+//Import Modules
 const nodemailer = require('nodemailer');
 const express = require('express');
 const cron = require('node-cron');
-// var $ = require('jQuery');
+const bodyParser = require('body-parser')
+const firebase = require('firebase');
+const path = require('path');
+
+//Initiaize Express and static files
 var app = express();
+app.use('/static', express.static('public'));
+
+//Initialize Body Parser Module
+app.use( bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+})); 
+
+//Initialize Port variable
 var port = process.env.PORT || 8080;
 
-// var admin = require('firebase-admin');
-var firebase = require('firebase');
-var serviceAccount = require('./config/serviceAccountKey.json');
 
+//Configure Firebase App
 var config = {
     apiKey: "AIzaSyCd52jgPUaWnYQ52g4EOHFgAk5F-_gajBg",
     authDomain: "teacher-monitoring-syste-2a943.firebaseapp.com",
@@ -20,7 +30,10 @@ var config = {
     messagingSenderId: "397902686650"
 };
 firebase.initializeApp(config);
+const auth = firebase.auth();
 
+
+//Initialize NodeMailer Module for sending Mail
 var transporter = nodemailer.createTransport({
     host: "smtp-mail.outlook.com", // hostname
     secureConnection: false, // TLS requires secureConnection to be false
@@ -35,6 +48,8 @@ var transporter = nodemailer.createTransport({
     }
 });
 
+
+//Setup a Cron Job to automatically notify staff
 cron.schedule('0 20 9 * * *', function () {
     console.log('Poda');
     var database = firebase.database();
@@ -46,7 +61,7 @@ cron.schedule('0 20 9 * * *', function () {
             var timeDifference = Math.abs(lastDate.getTime() - currentDate.getTime());
             var dateDifference = Math.ceil(timeDifference / (1000 * 3600 * 24));
             
-            if (dateDifference <= 2) {
+            if (dateDifference <= 3) {
                 var subject = 'Sumbit final report';
                 var bodyText = `Submition: \n Event Name: ${data[key].eventName} 
                     \n Last Date: ${data[key].lastDate}\n
@@ -57,36 +72,15 @@ cron.schedule('0 20 9 * * *', function () {
                     <h4><b>Make sure you submit the event details to me by tomorrow</b></h4>`;
                 console.log(data[key].mailId);
                 sendMail(data[key].eventName, data[key].lastDate, data[key].mailId, bodyText, bodyHtml);
+                
             }
         }
+
+
     });
 });
 
-app.get('/', function (req, res) {
-    res.send("Poda");
-    var currentDate = new Date();
-    console.log("Hours " + currentDate.getHours());
-    console.log("Minutes " + currentDate.getMinutes());
-});
-
-app.get('/sendMail', function (req, res) {
-    var toMail = req.query.mail;
-    var eventName = req.query.eventName;
-    var lastDate = req.query.lastDate;
-    console.log('Request Accepted');
-
-    var mailText = `Registration: \n Event Name: ${eventName} 
-                        \n Last Date: ${lastDate}\n
-                        Make sure you collect the event details from me by tomorrow`;
-    var mailHtml = `<h2>Registration: </h2><br/>
-                    <h4> Event Name: ${eventName} </h4> 
-                    <h4> Last Date: ${lastDate} </h4>
-                    <h4><b>Make sure you collect the event details from me by tomorrow</b></h4>`;
-
-    sendMail(eventName, lastDate, toMail, mailText, mailHtml);
-
-});
-
+//Function to Send Mail to staff
 function sendMail(eventName, lastDate, toMail, bodyText, bodyHtml) {
     console.log('Sending mail');
     let mailOptions = {
@@ -105,6 +99,43 @@ function sendMail(eventName, lastDate, toMail, bodyText, bodyHtml) {
         console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
     });
 }
+
+
+//Requests for webpages
+app.get('/', function (req, res) {
+    
+    res.sendFile(path.join(__dirname + '/public/login.html'));
+    
+});
+
+app.post('/login', function(req, res){
+
+    console.log(body);
+    auth.getUser
+
+});
+
+
+//API Request to send mail to staff
+app.get('/sendMail', function (req, res) {
+    var toMail = req.query.mail;
+    var eventName = req.query.eventName;
+    var lastDate = req.query.lastDate;
+    console.log('Request Accepted');
+
+    var mailText = `Registration: \n Event Name: ${eventName} 
+                        \n Last Date: ${lastDate}\n
+                        Make sure you collect the event details from me by tomorrow`;
+    var mailHtml = `<h2>Registration: </h2><br/>
+                    <h4> Event Name: ${eventName} </h4> 
+                    <h4> Last Date: ${lastDate} </h4>
+                    <h4><b>Make sure you collect the event details from me by tomorrow</b></h4>`;
+
+    sendMail(eventName, lastDate, toMail, mailText, mailHtml);
+
+});
+
+
 
 app.listen(port, function () {
     console.log("Server Started");
